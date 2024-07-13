ecfd16228e17218b7574c2a44eaa23df72d8f375
Gileno <gileno.cr.duarte@gmail.com>
2024-07-13 12:06:28 -0300

    estilizando agenda e criando cadastro dos eventos

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -18,9 +18,10 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return redirect('/')
+
+
 def submit_login(request):
     if request.POST:
-        print("ENTREI NO POST")
         username= request.POST.get('username')
         password= request.POST.get('password')
         usuario = authenticate(username=username, password=password)
@@ -32,9 +33,34 @@ def submit_login(request):
             messages.error(request, "Usuario ou senha invalido")
     return redirect('/')
 
+
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario= request.user
     evento = Evento.objects.filter(usuario=usuario)
     dados={'eventos': evento}
     return render(request,'agenda.html', dados)
+
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request,'evento.html')
+
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    print(request)
+    if request.POST:
+        print("Entrei no metodo post")
+        titulo= request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricacao= request.POST.get('descricao')
+        usuario= request.user
+        print("DADOS: ", titulo, data_evento,descricacao,usuario)
+        try:
+            Evento.objects.create(titulo=titulo, data_evento=data_evento,descricao=descricacao, usuario=usuario)
+        except Exception as e:
+            messages.error(request, "Erro ao cadastrar novo evento", e)
+
+        return redirect('/')
+    return redirect('/')
