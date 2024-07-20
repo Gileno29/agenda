138648125aab2c5a640e254e0efa97eae19829f5
Gileno <gileno.cr.duarte@gmail.com>
2024-07-20 10:59:16 -0300

    correção no deploy

--- agenda/agenda/settings.py
+++ agenda/agenda/settings.py
@@ -34,7 +34,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG =True #os.getenv('DEBUG', 'False') == 'True'
+DEBUG =False #os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']#os.getenv('ALLOWED_HOSTS').split(',')
 #CSRF_TRUSTED_ORIGINS = ['*']
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "static/"
 print("Caminho dos arquivos staticos: ", STATIC_ROOT)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 #STATICFILES_DIRS = [
  #   BASE_DIR / "static",
